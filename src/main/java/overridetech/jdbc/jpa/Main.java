19134975d3ebca863d1852380eac916a63b0afa0
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-03-19 22:32:25 +0300

    refactoring

--- src/main/java/overridetech/jdbc/jpa/Main.java
+++ src/main/java/overridetech/jdbc/jpa/Main.java
@@ -9,18 +9,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String DRIVER = "org.postgresql.Driver";
-        String DB_URL = "jdbc:postgresql://127.0.0.1:5432/test";
-        String USER = "postgres";
-        String PASS = "postgres";
-        Util.setConfig(DRIVER, DB_URL, USER, PASS);
 
         UserService userService = new UserServiceImpl();
 
-        userService.createUsersTable();
-
+//        userService.createUsersTable();
+        System.out.println("TABLICA SOZDANA");
         userService.saveUser("serg","chs", (byte) 24);
         System.out.println("User с именем – \"serg\" добавлен в базу данных");
+
         userService.saveUser("serg2","chs", (byte) 2);
         System.out.println("User с именем – \"serg2\" добавлен в базу данных");
         userService.saveUser("serg3","chs", (byte) 15);
--- src/main/java/overridetech/jdbc/jpa/service/UserServiceImpl.java
+++ src/main/java/overridetech/jdbc/jpa/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package overridetech.jdbc.jpa.service;
 
 import overridetech.jdbc.jpa.dao.UserDao;
+import overridetech.jdbc.jpa.dao.UserDaoHibernateImpl;
 import overridetech.jdbc.jpa.dao.UserDaoJDBCImpl;
 import overridetech.jdbc.jpa.model.User;
 import overridetech.jdbc.jpa.util.Util;
@@ -9,7 +10,9 @@ import java.sql.Connection;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDao userDao = new UserDaoJDBCImpl();
+//    private final UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoHibernateImpl();
+
 
     public void createUsersTable() {
         userDao.createUsersTable();
